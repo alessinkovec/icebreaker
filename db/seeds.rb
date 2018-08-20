26e33c559e587b64c15314a51d9bff6d814555f8
Ales Sinkovec <ales@parity.solutions>
2018-08-20 18:33:33 -0300

    made seed v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all garbage and you know it"
+
+User.destroy_all
+Event.destroy_all
+
+puts "Creating all motherfuckers"
+
+
+gender = ["male", "female"]
+address_people = [
+  "WeWork Ipanema, Visconde de Piraja 495, Rio",
+  "Rua Canning 22, Ipanema"
+]
+
+20.times do User.create!(
+  first_name: Faker::Name.first_name
+  last_name: Faker::Name.last_name
+  address: address_people.sample
+  gender: gender.sample
+  birthday: Faker::Date.birthday(18, 42)
+  email: Faker::Internet.email
+  password: "123456"
+  )
+end
+
+address_places = [
+  "Blue Agave, Ipanema",
+  "Sabugosa, Ipanema",
+  "Polis Sucos, Ipanema",
+  "Centaurus, Ipanema",
+  "Azumi, Copacabana",
+  "Siqueiro Campos Metro, Copacabana"
+]
+
+4.times do Event.create!(
+  name: Faker::StrangerThings.quote
+  address: adress_places.sample
+  time: Faker::Time.between(DateTime.now, Date.today, :evening)
+  user_id: rand(1..20)
+  )
+end
+
+
+puts "Finished the motherfuckening"
+
+
+
+
+# Users
+# first name
+# last name
+# address
+# gender
+# email
+# password
+
+
+# Events
+# name
+# address
+# time
+# status
+# user_id
