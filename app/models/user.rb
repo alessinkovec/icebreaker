f354043ee6d34c27075a475285d3ca5abb3d8be2
Adam Sinning <asinning25@gmail.com>
2018-08-21 11:03:27 -0300

    references changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events
+  belongs_to :event
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
