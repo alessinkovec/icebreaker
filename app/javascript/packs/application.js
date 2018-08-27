8ee1fab6a2e1b46bcca7dc6c19f3c76c8de1bbb1
Francisco Braga <franciscobragafernandes@gmail.com>
2018-08-27 19:25:49 -0300

    Add particle to button on index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,14 @@ import "bootstrap";
 import { flipcard } from "../components/flipcard.js"
 import { scroll } from "../components/scroll.js"
 import { scroll2 } from "../components/scroll.js"
+import anime from 'animejs'
+// import { particula } from '../components/particles.js'
 
 if (document.querySelector('.flip-container')) flipcard();
 
 if (document.querySelector('#scroll')) scroll();
 if (document.querySelector('.scroll2')) scroll2();
+
+
+
 
